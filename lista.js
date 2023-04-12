3f42c6bb90ef765725b74e3c3e863f89b397eb91
KakoCaio <kako.caio16@gmail.com>
2023-04-12 19:55:21 -0300

    primeira aula de js

---
+++ lista.js
@@ -0,0 +1,11 @@
+let text = "marcos"
+let num = 5
+let pers = {principal:"kit"}
+console.log (pers.principal)
+const lista = [text, num, pers,num+pers.principal]
+console.log(lista)
+console.log(lista[2])
+console.log(lista[0])
+console.log(lista[1])
+console.log(lista[2].principal)
+console.log(lista[3])
---
+++ teste.js
@@ -0,0 +1,12 @@
+console.log("começando o programa")
+let x = 10;
+// Here x is 10
+console.log(x)
+
+{
+let x = 2;
+console.log(x)
+// Here x is 2
+}
+
+// Here x is 10 
