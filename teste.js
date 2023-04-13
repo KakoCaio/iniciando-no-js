2ba41326320cc71a10d9f270deaf32d61b4b878d
KakoCaio <kako.caio16@gmail.com>
2023-04-13 20:02:37 -0300

    estudo sobre function + if else

--- teste.js
+++ teste.js
@@ -10,3 +10,22 @@ console.log(x)
 }
 
 // Here x is 10 
+let y = Math.ceil(4.9);
+console.log(y)
+let z = Math.ceil(4.7);
+console.log(z)
+let a = Math.round(4.4);
+let b = Math.ceil(4.2);
+let c = Math.ceil(-4.2);
+console.log(c)
+console.log(a)
+let i = 2;
+while (i < 10) {
+  console.log(i); 
+i = i + 2;
+}
+
+
+
+
+
